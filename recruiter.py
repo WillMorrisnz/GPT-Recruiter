3c761daf3aaa6d92dccfc90e6df995d7e59422ad
William Morris <ewm27@uclive.ac.nz>
2024-02-04 01:14:23 +1100

    Implemented scrape service

--- recruiter.py
+++ recruiter.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import scrape_service
+import json
 
 
 load_dotenv()
@@ -36,11 +38,15 @@ def evaluate_cv_against_job_description(cv, job_description, evaluation_categori
       messages=[
           {
             "role": "system",
-            "content": f"""You are a extremly experienced recruiter with an outstanding knowledge of the tech industry. \
-              Your task is to evaluate the experience and suitability of the applicant for the provided job description using information from the applicants cv. 
-              Please only respond with a rating for the applicant out of 5 for each of the following categories: 
-              {evaluation_categories_string}
-              Your response should be in json format with a rating and short reasoning for the rating. e.g.{{"Technical Skills": {{rating : 4, reasoning : ... }}}}"""},
+            "content": f"""As an extremely experienced recruiter with an outstanding knowledge of the tech industry, your task is to accurately evaluate the applicant's experience and suitability for the provided job description. Utilize the information from the applicant's CV to assess their fit across the following categories: {evaluation_categories_string}. 
+            Please format your response in raw JSON, providing a rating on a scale of 1-5 and a brief reasoning for each category. For example, suppose the categories are Technical Skills, Communication Skills, and Teamwork. In that case, your response should resemble the following structure:
+            {{ 
+              "Technical Skills": {{ "rating": 4, "reasoning": "Has extensive experience with Java and Python, but lacks experience with cloud technologies which the job requires." }},
+              "Communication Skills": {{ "rating": 5, "reasoning": "Demonstrated excellent communication skills through various team projects and presentations." }},
+              "Teamwork": {{ "rating": 3, "reasoning": "CV highlights individual achievements well but lacks examples of collaborative projects." }}
+            }}
+            """
+          },
           {
               "role": "user",
               "content": f"CV: {cv} \n Job Description: {job_description}",
@@ -59,7 +65,23 @@ evaluation_categories = [
     'Soft Skills and Cultural Fit', 
     'Relevant Experience',
     ]
-job_description = read_file_content('job_description.txt')
 cv = read_file_content('cv.txt')
 
-print(evaluate_cv_against_job_description(cv, job_description, evaluation_categories))
+
+def evaluate_seek_job_listings():
+  job_data_dict = scrape_service.scrape_seek_job_data(1)
+
+  for key in job_data_dict:
+    job_description = f"""
+      title: {job_data_dict[key]['title']}
+      company: {job_data_dict[key]['advertiser']}
+      work_type: {job_data_dict[key]['work_type']}
+      description: {job_data_dict[key]['description']}
+      """
+
+    job_fit_evaluation = evaluate_cv_against_job_description(cv, job_description, evaluation_categories)
+    job_data_dict[key]['job_fit_evaluation'] = job_fit_evaluation
+    print(key)
+    print(json.dumps(job_data_dict[key], indent=2))
+
+evaluate_seek_job_listings()
---
+++ scrape_service.py
@@ -0,0 +1,140 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from scrapingbee import ScrapingBeeClient
+from dotenv import load_dotenv
+import os
+from bs4 import BeautifulSoup
+import re
+
+
+load_dotenv()
+API_KEY = os.getenv('SCRAPING_BEE_API_KEY')
+
+def extract_text_by_data_automation(html_content, attribute_value, element_type):
+    # Parse the HTML content
+    soup = BeautifulSoup(html_content, 'lxml')
+    
+    # Find the <element> by data-automation attribute
+    element = soup.find(element_type, attrs={'data-automation': attribute_value})
+    
+    # Return the text from the element, or None if the element wasn't found
+    return element.get_text(strip=True) if element else None
+
+
+# -------------------------
+
+
+def parallel_proxy_scrape_with_retries(urls):
+    client = ScrapingBeeClient(api_key=API_KEY)
+    html_data = []
+    MAX_RETRIES = 3
+
+    print(f"{len(urls)} URLS TO SCRAPE")
+
+    def scrape(url):
+        for attempt in range(MAX_RETRIES):
+            response = client.get(url, params={'render_js': False})
+            if response.ok:
+                return (url, response.content)
+            else:
+                print(f"Attempt {attempt + 1} failed : Status {response.status_code}")
+        return None
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_url = {executor.submit(scrape, url): url for url in urls}
+        for future in as_completed(future_to_url):
+            print(f"{len(html_data)} / {len(urls)}")
+            result = future.result()
+            if result is not None:
+                html_data.append(result)
+
+    return html_data
+
+
+# -------------------------
+
+
+def construct_seek_search_link(search_term='Engineer', job_category='information-communication-technology', search_distance='10'):
+    return f'https://www.seek.co.nz/{search_term}-jobs-in-{job_category}/in-Abbotsford-VIC-3067-AU?distance={search_distance}'
+
+
+# -------------------------
+
+
+def scrape_job_page_links_from_seek(seek_search_link, num_pages=1):
+    urls = []
+    for page in range(0, num_pages):
+        urls.append(f"{seek_search_link}&page={page}") 
+    job_listings = parallel_proxy_scrape_with_retries(urls)
+    
+    job_urls = set() 
+    for job in job_listings:
+        job_url = extract_job_urls(job[1]) 
+        job_urls.update(job_url) 
+
+    return list(job_urls)
+
+
+# -------------------------
+
+
+def extract_job_urls(html_content):
+    soup = BeautifulSoup(html_content, 'lxml')
+    a_tags = soup.find_all('a', href=True)
+    
+    # Extract URLs that contain '/job/'
+    job_urls = [a['href'] for a in a_tags if '/job/' in a['href']]
+    
+    base_url = "https://www.seek.co.nz"
+    full_urls = [base_url + url for url in job_urls]
+    
+    return full_urls
+
+
+# -------------------------
+
+
+def extract_job_id_from_url(url):
+    match = re.search(r'/job/(\d+)', url)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+
+# -------------------------
+
+
+def scrape_job_descriptions(job_page_links):
+    job_listing_data = dict()
+    html_content = parallel_proxy_scrape_with_retries(job_page_links)
+    for scrape_content in html_content:
+        url = scrape_content[0]
+        page = scrape_content[1]
+
+        job_id = extract_job_id_from_url(url)
+        job_description = extract_text_by_data_automation(page, 'jobAdDetails', 'div')
+        job_title = extract_text_by_data_automation(page, 'job-detail-title', 'h1')
+        job_advertiser = extract_text_by_data_automation(page, 'advertiser-name', 'span')
+        job_work_type = extract_text_by_data_automation(page, 'job-detail-work-type', 'span')
+
+        # Storing the scraped data in the dictionary
+        job_listing_data[job_id] = {
+            'description': job_description,
+            'title': job_title,
+            'advertiser': job_advertiser,
+            'work_type': job_work_type,
+            'url': url
+        }
+
+    return job_listing_data
+
+
+# -------------------------
+
+
+def scrape_seek_job_data(num_pages=1):
+    seek_search_link = construct_seek_search_link()
+    job_page_links = scrape_job_page_links_from_seek(seek_search_link, num_pages)
+    job_data_dict = scrape_job_descriptions(job_page_links)
+    return job_data_dict
+
